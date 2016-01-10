a1181cc71430f0382c0dc7b3d46dd457bfe52c33
Panu Kalliokoski <panu.kalliokoski@server.fake>
2016-01-10 20:09:14 +0200

    Sprite class

--- whop.js
+++ whop.js
@@ -5,10 +5,10 @@ var whop = whop || (function () {
 
 var node = document.createElement.bind(document);
 var elem = document.getElementById.bind(document);
-var $$ = document.querySelectorAll.bind(document);
 var stage;
+var sprites = [];
 
-function move(n, x, y) {
+function movenode(n, x, y) {
   n.style.left = x + 'px';
   n.style.top = y + 'px';
 }
@@ -17,17 +17,43 @@ function img(src, x, y) {
   var n = node('img');
   n.src = src;
   n.style.position = 'absolute';
-  move(n, x, y);
+  movenode(n, x, y);
   stage.appendChild(n);
   return n;
+}
+
+function makeSprite(src, x, y) {
+  var xd = 0, yd = 0;
+  var node = img(src, x, y);
+
+  function move(nx, ny) {
+    x = nx;
+    y = ny;
+    movenode(node, x, y);
+  }
+
+  function update() { move(x + xd, y + yd); }
+  function setSpeed(nxd, nyd) { xd = nxd; yd = nyd; }
+  function accel(xdd, ydd) { setSpeed(xd + xdd, yd + ydd); }
+
+  var inst = { move: move, update: update, setSpeed: setSpeed, accel: accel };
+  sprites.push(inst);
+
+  return inst;
+}
+
+function nextFrame() {
+  sprites.forEach(function(sprite) { sprite.update(); });
+  requestAnimationFrame(nextFrame);
 }
 
 function init() {
   stage = elem('game');
   stage.removeChild(elem('jswarning'));
+  requestAnimationFrame(nextFrame);
 }
 
-return { img: img, initialise: init, move: move };
+return { initialise: init, makeSprite: makeSprite };
 
 })();
 
