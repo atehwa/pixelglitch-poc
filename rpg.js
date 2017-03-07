61b0c80ceee3e93f4cce7490ee30bf923d225f5d
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2017-03-07 20:49:34 +0200

    setup for background and lucy

---
+++ rpg.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var bg;
+var lucy;
+
+function rpg_initialise() {
+	bg = whop.makeSprite("img/battle-bg.png",0,0);
+	lucy = whop.makeSprite("img/LucyBattleSprite.png",120-32,80-32);
+}
+
