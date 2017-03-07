f72f6c67756fbc61b17d57d64d9c215786cc3757
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2017-03-07 21:14:21 +0200

    Set up RPG stage

--- rpg.js
+++ rpg.js
@@ -1,10 +1,12 @@
 "use strict";
 
-var bg;
-var lucy;
+var bg, oppo, lucy, bar;
 
 function rpg_initialise() {
 	bg = whop.makeSprite("img/battle-bg.png",0,0);
+	oppo = whop.makeSprite("img/Cheforoni1.png", 120-32, 0);
 	lucy = whop.makeSprite("img/LucyBattleSprite.png",120-32,80-32);
+	lucy.setGhost(0.5);
+	bar = whop.makeSprite('img/battle-bar.png',0,160-48);
 }
 
